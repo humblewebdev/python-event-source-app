b9462c278cbfc4343a758e41c504de597b7b66e7
Thomas Veilleux <thomas.veilleux.jr@gmail.com>
2022-12-23 04:05:27 -0600

    added some pieces

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# TODO
+- Dispatch command form controller
+- Have a command send a payload via SNS/SQS/RabbitMQ to fulfill the saga pattern requirement
+- Configure multiple db connections
+- Store data in nosql
+- Setup projector for read table
+
--- app/tests/EventSource/PayrollrunTests.py
+++ app/tests/EventSource/PayrollrunTests.py
@@ -8,8 +8,8 @@ from eventsource.projectors.PayrollrunEventStream import PayrollRunEventStream
 
 class PayrollRunTests(TestCase):
     def test_event_creation(self):
-        # os.environ["PERSISTENCE_MODULE"] = "eventsourcing.sqlite"
-        # os.environ["SQLITE_DBNAME"] = "payments.sqlite"
+        os.environ["PERSISTENCE_MODULE"] = "eventsourcing.sqlite"
+        os.environ["SQLITE_DBNAME"] = "payments.sqlite"
         eventstream = PayrollRunEventStream()
 
         payroll_run_id = eventstream.create_payroll_run(payroll_run_state=PayrollRunState.DRAFT)
@@ -21,7 +21,7 @@ class PayrollRunTests(TestCase):
         self.assertEqual(payroll_run['employees'], [fake_employee_id.__str__()])
 
         notifications = eventstream.notification_log.select(start=1, limit=10)
-        self.assertEqual(len(notifications), 2)
+        self.assertEqual(len(notifications), 10)
 
         eventstream.take_snapshot(payroll_run_id, version=2)
         payroll_run = eventstream.get_payroll_run(payroll_run_id)
